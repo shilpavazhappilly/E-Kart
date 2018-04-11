0e92f67e3b71c2518a6a1001f048908454e6c9db
Shilpa V Pius <shilpavpius@gmail.com>
2018-04-11 12:42:31 +0530

    Intial Commit

---
+++ MayFlowerShoppingCart/MayFlowerShoppingCart/ViewController/RegisterViewController.swift
@@ -0,0 +1,25 @@
+//
+//  RegisterViewController.swift
+//  MayFlowerShoppingCart
+//
+//  Created by globesoftmac2 on 4/11/18.
+//  Copyright © 2018 shilpa.com. All rights reserved.
+//
+
+import UIKit
+
+class RegisterViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+}
---
+++ MayFlowerShoppingCart/MayFlowerShoppingCart/ViewController/ViewController.swift
@@ -0,0 +1,38 @@
+//
+//  ViewController.swift
+//  MayFlowerShoppingCart
+//
+//  Created by globesoftmac2 on 4/10/18.
+//  Copyright © 2018 shilpa.com. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var userNameTextField: UITextField!
+    
+    @IBOutlet weak var passwordTextField: UITextField!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+           }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        
+    }
+
+    @IBAction func forgotPasswordTapped(_ sender: Any) {
+    }
+
+    
+    @IBAction func loginButtonTapped(_ sender: Any) {
+    }
+    
+    @IBAction func registerTapped(_ sender: Any) {
+        
+        let vc = storyboard?.instantiateViewController(withIdentifier: "RegisterViewController")
+        self.present(vc!, animated: true, completion: nil)
+            }
+}
+
