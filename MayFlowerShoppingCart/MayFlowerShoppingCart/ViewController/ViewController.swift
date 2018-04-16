7d80d89bf85b2d9e7a81a8db494179ae30e90862
Shilpa V Pius <shilpavpius@gmail.com>
2018-04-16 13:00:56 +0530

    login credentials verification

--- MayFlowerShoppingCart/MayFlowerShoppingCart/ViewController/ViewController.swift
+++ MayFlowerShoppingCart/MayFlowerShoppingCart/ViewController/ViewController.swift
@@ -28,73 +28,9 @@ class ViewController: UIViewController {
 
     
     @IBAction func loginButtonTapped(_ sender: Any) {
-        let parameters = ["username":userNameTextField.text!,"password":passwordTextField.text!] as Dictionary<String, String>
+        postLoginCall(url: "13.229.125.8:8081/api/login")
         
-        //create the url with URL
-        let url = URL(string: "13.229.125.8:8081/api/login")! //change the url
-        
-        //create the session object
-        let session = URLSession.shared
-        
-        //now create the URLRequest object using the url object
-        var request = URLRequest(url: url)
-        request.httpMethod = "POST" //set http method as POST
-        
-        do {
-            request.httpBody = try JSONSerialization.data(withJSONObject: parameters, options: .prettyPrinted) // pass dictionary to nsdata object and set it as request body
-            
-        } catch let error {
-            print(error.localizedDescription)
-        }
-        
-        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
-        request.addValue("application/json", forHTTPHeaderField: "Accept")
-        
-        //create dataTask using the session object to send data to the server
-        let task = session.dataTask(with: request as URLRequest, completionHandler: { data, response, error in
-            
-            guard error == nil else {
-                return
-            }
-            
-            guard let data = data else {
-                return
-            }
-            
-            do {
-                //create json object from data
-                if let json = try JSONSerialization.jsonObject(with: data, options: .mutableContainers) as? [String: Any] {
-                    print(json)
-                    if let status = json["status"] as? String
-                    {
-                        if(status == "success")
-                        {
-                            self.performSegue(withIdentifier: "ProductListViewController", sender: self)
-                            
-                            print("yes")
-                        }else{
-                                                     self.showAlertView(message: "username&password are required", viewController: self)                            }
-                    }
-                    // handle json...
-                }
-                
-            } catch let error {
-                print(error.localizedDescription)
-            }
-        })
-        task.resume()
     }
-    func showAlertView(message: String, viewController : UIViewController )
-    {
-        let alertController = UIAlertController(title: "", message: message, preferredStyle: UIAlertControllerStyle.alert) //Replace UIAlertControllerStyle.Alert by UIAlertControllerStyle.alert
-        let okAction = UIAlertAction(title: "OK", style: UIAlertActionStyle.default) { (result : UIAlertAction) -> Void in
-            print("OK")
-        }
-        alertController.addAction(okAction)
-        viewController.present(alertController, animated: true, completion: nil)
-    }
-
-    
 
     @IBAction func registerTapped(_ sender: Any) {
         
@@ -102,4 +38,81 @@ class ViewController: UIViewController {
         self.present(vc!, animated: true, completion: nil)
             }
 }
+
+// Mark:- Login
+extension ViewController{
+    func postLoginCall(url : String){
+        
+        
+        let request = NSMutableURLRequest(url: NSURL(string: url)! as URL)
+        request.httpMethod = "POST"
+        let postString = "emailaddress=\(userNameTextField.text!)&password=\(passwordTextField.text!)"
+        print(postString)
+        request.setValue("application/x-www-form-urlencoded; charset=utf-8", forHTTPHeaderField: "Content-Type")
+        request.httpBody = postString.data(using: String.Encoding.utf8)
+        
+        let task = URLSession.shared.dataTask(with: request as URLRequest) { data, response, error in
+            guard error == nil && data != nil else {                                                          // check for fundamental networking error
+                print("error=\(error)")
+                return
+            }
+            
+            do {
+                if let responseJSON = try JSONSerialization.jsonObject(with: data!) as? [String:AnyObject]{
+                    print(responseJSON)
+                    print(responseJSON["status"]!)
+                    
+                    
+                   let response1 = responseJSON["status"]! as! Int
+                    
+                    print(response1)
+                    
+                    //Check response from the sever
+                    if response1 == 200
+                    {
+                        OperationQueue.main.addOperation {
+                            
+                            //API call Successful and can perform other operatios
+                            print("Login Successful")
+                            let vc = self.storyboard?.instantiateViewController(withIdentifier: "ProductListViewController")
+                            self.present(vc!, animated: true, completion: nil)
+                        }
+                        
+                    }
+                        
+                    else
+                    {
+                        OperationQueue.main.addOperation {
+                            
+                            //API call failed and perform other operations
+                            print("Login Failed")
+                            let alert  = UIAlertController(title: "Login Failed", message:"Invalid Credentials", preferredStyle: .alert)
+                            let cancelAction = UIAlertAction(title: "CANCEL", style: .destructive, handler: nil)
+                            alert.addAction(cancelAction)
+                            self.present(alert, animated: true, completion: nil)
+
+                            
+                        }
+                        
+                    }
+                    
+                    
+                }
+            }
+            catch {
+                print("Error -> \(error)")
+            }
+            
+            
+            
+        }
+        
+        
+        task.resume()
+        
+        
+        
+    }
+}
+
 
